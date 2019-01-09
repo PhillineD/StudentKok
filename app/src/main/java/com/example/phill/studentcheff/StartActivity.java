efbacdaa1937a3777a060204ce9de0e2cfdc369c
Philline <fidik@live.nl>
2019-01-09 09:15:25 +0100

    Initial Project

---
+++ app/src/main/java/com/example/phill/studentcheff/StartActivity.java
@@ -0,0 +1,22 @@
+package com.example.phill.studentcheff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_activity);
+    }
+
+    public void clicknew(View view) {
+    }
+
+
+    public void clickhistory(View view) {
+    }
+
+}
