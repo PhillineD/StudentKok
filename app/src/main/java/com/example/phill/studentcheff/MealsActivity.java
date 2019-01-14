74ba9ed9be4a6d4a74e5d202276ea1d2f2cffa3d
Philline <fidik@live.nl>
2019-01-14 17:51:56 +0100

    Initial Project

--- app/src/main/java/com/example/phill/studentcheff/MealsActivity.java
+++ app/src/main/java/com/example/phill/studentcheff/MealsActivity.java
@@ -3,17 +3,29 @@ package com.example.phill.studentcheff;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MealsActivity extends AppCompatActivity implements MealsRequest.Callback{
+    EditText textBox;
+    List<String> searchlist= new ArrayList<>();
+    String lijstje[];
+    ArrayAdapter adapter1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,13 +39,21 @@ public class MealsActivity extends AppCompatActivity implements MealsRequest.Cal
     }
 
     @Override
-    public void gotMeals(ArrayList<MealItem> meals) {
+    public void gotMeals(final ArrayList<MealItem> meals) {
 //        Toast.makeText(this, meals.get(0),Toast.LENGTH_LONG).show();
         Log.d("gotmeals", "gotMeals: we zijn er "+ meals);
 
-        MealAdapter adapter = new MealAdapter(this, R.layout.meals_activity, meals);
+        final MealAdapter adapter = new MealAdapter(this, R.layout.meals_activity, meals);
         GridView listView =findViewById(R.id.MealsListView);
         listView.setAdapter(adapter);
+
+        for(int i=0;i<meals.size();i++){
+            searchlist.add(meals.get(i).getitle());
+        }
+
+//        adapter1 = new ArrayAdapter(this,R.layout.meals_item,searchlist);
+
+        Log.d("gotmeals", "honger "+ searchlist );
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
 
@@ -47,6 +67,25 @@ public class MealsActivity extends AppCompatActivity implements MealsRequest.Cal
                 // alle waardes moeten doorgegeven worden, dan hoeven ze alleen geplaatst te worden.
                 startActivity(choosen);
                 finish();
+            }
+        });
+
+        textBox = (EditText) findViewById(R.id.editText);
+        textBox.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                Log.d("typen", "beforeTextChanged: getypt"   + meals.get(0).getitle());
+                adapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
 
