58c7a505531265e8fd902fd560e413fea4c5f626
Philline <fidik@live.nl>
2019-01-22 14:29:43 +0100

    Initial Project

--- app/src/main/java/com/example/phill/studentcheff/Activitys/StartActivity.java
+++ app/src/main/java/com/example/phill/studentcheff/Activitys/StartActivity.java
@@ -12,10 +12,16 @@
 
 package com.example.phill.studentcheff.Activitys;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import com.example.phill.studentcheff.R;
 
@@ -47,4 +53,30 @@ public class StartActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+
+    public void informationClick(View view) {// inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_history, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+
+    }
 }
